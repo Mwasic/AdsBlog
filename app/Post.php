5bf0ae20d68b40769b1adab42e12b1ae19b30782
Mwasic <mwakam95@gmail.com>
2019-04-05 16:16:23 +0300

    created posts folder with an index file

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    //Table name
+    protected $table = 'posts';
+
+    //Primary Key
+    public $primayKey = 'id';
+
+    //Timestamps
+    public $timestamps = 'true';
+    
 }
