f2d5e47e90d25d7fb61fc74a168ac856c349c494
Mwasic <mwakam95@gmail.com>
2019-04-16 17:20:37 +0300

    adding navbar update

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,4 +1,4 @@
-    
+{{--     
     <nav class="navbar navbar-inverse">
         <div class="container">
             <div class="navbar-header">
@@ -20,6 +20,30 @@
                     <li><a href="/posts">Blog</a></li>
 
                 </ul>
-            </div>        {{-- nav collapse --}}
+            </div>        
         </div>
-    </nav>
+    </nav> --}}
+
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+        <a class="navbar-brand" href="/">AdsBlog</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="/">Home </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/about">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/posts">Blog</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/services">Services</a>
+              </li>
+          </ul>
+         
+        </div>
+      </nav>
